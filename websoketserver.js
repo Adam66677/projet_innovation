d39ed8a1b44dbd03622a3aaf85b48680182b1ff5
Ayyoubi <114993701+ayyoubiyouness@users.noreply.github.com>
2022-12-04 13:39:49 +0100

    Create websoketserver.js

---
+++ websoketserver.js
@@ -0,0 +1,12 @@
+const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({
+    port: 9876
+})
+
+wss.on('connection', function(ws) {
+    ws.on('message', function(Demarrer) {
+        ws.send(Demarrer)
+    })
+})
+
